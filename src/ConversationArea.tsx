04a22d57e4108b9c33ab327f77f561a7186a752e
Adam Liljenberg <adam@cabanossy.se>
2024-03-07 13:26:56 +0100

    Refactor ConversationArea component to display messages and placeholders

--- src/ConversationArea.tsx
+++ src/ConversationArea.tsx
@@ -1,7 +1,25 @@
-const ConversationArea = () => {
+import ConversationPlaceHolder from "./ConversationPlaceHolder";
+import MessageBox from "./MessageBox";
+
+interface ConversationAreaProps {
+  messages: string[];
+}
+
+const ConversationArea = (props: ConversationAreaProps) => {
+  const { messages } = props;
+
   return (
-    <div className="w-full flex justify-center overflow-scroll">
-      <div className="w-[50rem] shrink">Chat Header</div>
+    <div className="w-full flex flex-col-reverse items-center overflow-scroll">
+      <div className="w-[50rem] shrink">
+        {messages.map((message, index) => (
+          <MessageBox
+            message={message}
+            isChatGbg={index % 2 === 1}
+            key={index}
+          />
+        ))}
+        {messages.length === 0 && <ConversationPlaceHolder />}
+      </div>
     </div>
   );
 };
