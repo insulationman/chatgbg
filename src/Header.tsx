d8a40863439a29a9148ae30270557cfcea88ae33
Adam Liljenberg <adam@cabanossy.se>
2024-03-07 12:54:12 +0100

    Added header

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,5 +1,13 @@
+import SellaLogo from "./assets/logo.svg";
+
 const Header = () => {
-  return <div className="">ChatGBG</div>;
+  return (
+    <div className="flex w-full items-center p-4 gap-2">
+      <h1 className="text-2xl font-bold">ChatGBG</h1>
+      <span className="ml-auto">En produkt av</span>
+      <img src={SellaLogo} alt="Sella" className="w-20" />
+    </div>
+  );
 };
 
 export default Header;
