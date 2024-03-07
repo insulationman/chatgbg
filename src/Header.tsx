cf5a84ad94252111ac36d8abd0d6bb28250796b1
Adam Liljenberg <adam@cabanossy.se>
2024-03-07 18:50:10 +0100

    Update header in ChatGBG component

--- src/Header.tsx
+++ src/Header.tsx
@@ -3,9 +3,11 @@ import CabLogo from "./assets/logo.svg";
 const Header = () => {
   return (
     <div className="flex w-full items-center p-4 gap-2">
-      <h1 className="text-2xl font-bold">ChatGBG</h1>
+      <h1 className="text-2xl font-bold">
+        ChatGBG <span className=" text-blue-400">4.1</span>
+      </h1>
       <span className="ml-auto"></span>
-      <img src={CabLogo} alt="Sella" className="w-20" />
+      <img src={CabLogo} alt="Sella" className="w-32" />
     </div>
   );
 };
