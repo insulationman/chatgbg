bfa8f5ae81850fad57273f1d7c328914b66833da
Adam Liljenberg <adam@cabanossy.se>
2024-03-07 18:21:10 +0100

    Update logo import in Header component

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,11 +1,11 @@
-import SellaLogo from "./assets/logo.svg";
+import CabLogo from "./assets/logo.svg";
 
 const Header = () => {
   return (
     <div className="flex w-full items-center p-4 gap-2">
       <h1 className="text-2xl font-bold">ChatGBG</h1>
-      <span className="ml-auto">En produkt av</span>
-      <img src={SellaLogo} alt="Sella" className="w-20" />
+      <span className="ml-auto"></span>
+      <img src={CabLogo} alt="Sella" className="w-20" />
     </div>
   );
 };
