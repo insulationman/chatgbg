0f9ded395fb8170647bd8cb56a85a6593e5d5dd0
Adam Liljenberg <adam@cabanossy.se>
2024-03-07 18:20:21 +0100

    Add typed.js animation to MessageBox component

--- src/MessageBox.tsx
+++ src/MessageBox.tsx
@@ -1,16 +1,35 @@
+import { memo, useEffect, useRef } from "react";
+import Typed from "typed.js";
+
 interface MessageBoxProps {
   message: string;
   isChatGbg: boolean;
 }
 
-const MessageBox = (props: MessageBoxProps) => {
+const MessageBox = memo((props: MessageBoxProps) => {
   const { message, isChatGbg } = props;
+
+  const el = useRef(null);
+
+  useEffect(() => {
+    const typed = new Typed(el.current, {
+      strings: [message],
+      typeSpeed: isChatGbg ? 50 : 1,
+      showCursor: false,
+    });
+
+    return () => {
+      // Destroy Typed instance during cleanup to stop animation
+      typed.destroy();
+    };
+  });
+
   return (
     <div className="p-4 border-2 border-gray-200 rounded-lg mb-4">
       <p className="text-xs text-blue-400">{isChatGbg ? "ChatGBG" : "Du"}</p>
-      <p>{message}</p>
+      <span ref={el} />
     </div>
   );
-};
+});
 
 export default MessageBox;
