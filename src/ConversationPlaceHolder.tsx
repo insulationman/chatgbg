9585bb1e2e3ee7cbf74088441f2d0794272e829c
Adam Liljenberg <adam@cabanossy.se>
2024-03-07 13:22:22 +0100

    Add ConversationPlaceHolder component

---
+++ src/ConversationPlaceHolder.tsx
@@ -0,0 +1,11 @@
+const ConversationPlaceHolder = () => {
+  return (
+    <div>
+      <div className="flex justify-start p-4 border-2 border-gray-200 rounded-lg mb-4">
+        <p>Det finns inga meddelanden än</p>
+      </div>
+    </div>
+  );
+};
+
+export default ConversationPlaceHolder;
