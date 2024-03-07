b0b54795024ee2ae220311ffecf7e3d7eeb88e0f
Adam Liljenberg <adam@cabanossy.se>
2024-03-07 13:27:01 +0100

    Add state for messages in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
+import { useState } from "react";
 import ConversationArea from "./ConversationArea";
 import Header from "./Header";
 import InputArea from "./InputArea";
 
 function App() {
+  const [messages, setMessages] = useState<string[]>([]);
+
   return (
     <div className="flex flex-col h-screen">
       <Header />
-      <ConversationArea />
-      <InputArea onSend={(message) => console.log(message)} />
+      <ConversationArea messages={messages} />
+      <InputArea onSend={(message) => setMessages([...messages, message])} />
     </div>
   );
 }
