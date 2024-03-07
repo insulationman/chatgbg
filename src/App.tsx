680c38039aaa9f2c23433460f438386d18efc3a4
Adam Liljenberg <adam@cabanossy.se>
2024-03-07 18:08:13 +0100

    Add message response functionality

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,30 @@ import InputArea from "./InputArea";
 function App() {
   const [messages, setMessages] = useState<string[]>([]);
 
+  const handleSubmit = (message: string) => {
+    setMessages((prevMessages) => [...prevMessages, message]);
+    setMessages((prevMessages) => [...prevMessages, createResponse(message)]);
+  };
+
+  const createResponse = (message: string) => {
+    if (message.toLowerCase().includes("hello")) {
+      return "Hello there!";
+    } else if (message.toLowerCase().includes("how are you")) {
+      return "I'm doing great, thanks for asking!";
+    } else if (message.toLowerCase().includes("what's your name")) {
+      return "I'm a bot, I don't have a name!";
+    } else if (message.toLowerCase().includes("how old are you")) {
+      return "I'm a bot, I don't have an age!";
+    } else {
+      return "I'm sorry, I don't understand that.";
+    }
+  };
+
   return (
     <div className="flex flex-col h-screen">
       <Header />
       <ConversationArea messages={messages} />
-      <InputArea onSend={(message) => setMessages([...messages, message])} />
+      <InputArea onSend={handleSubmit} />
     </div>
   );
 }
