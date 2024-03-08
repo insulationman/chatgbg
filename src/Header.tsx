26434892ae9d2374414cbc76eb15eb1cedf4d814
Adam Liljenberg <adam@cabanossy.se>
2024-03-08 07:21:58 +0100

    Update version number in Header component

--- src/Header.tsx
+++ src/Header.tsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div className="flex w-full items-center p-4 gap-2">
       <h1 className="text-2xl font-bold">
-        ChatGBG <span className=" text-blue-400">4.1</span>
+        ChatGBG <span className=" text-blue-400">0.3.1</span>
       </h1>
       <span className="ml-auto"></span>
       <img src={CabLogo} alt="Sella" className="w-32" />
