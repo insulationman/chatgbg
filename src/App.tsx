b63b8135efd2f1688d4ecc1de06b07b871b91006
Adam Liljenberg <adam@cabanossy.se>
2024-03-08 07:20:51 +0100

    Update App.tsx styling

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ function App() {
   };
 
   return (
-    <div className="flex flex-col h-screen">
+    <div className="flex flex-col h-dvh">
       <Header />
       <ConversationArea messages={messages} />
       <InputArea onSend={handleSubmit} />
